14c7762b7d09978d648f1f667c0acb8390a35220
UmedaTakefumi <ut+github@azurestone.net>
2019-03-17 03:13:58 +0900

    sorting time.bash

--- src/time.bash
+++ src/time.bash
@@ -1,28 +1,36 @@
-# ISO8601形式で日付を取得する
-# 
-# see also:
-#   https://www.iso.org/standard/40874.html
-#   https://www.iso.org/standard/70907.html
-#   https://www.iso.org/standard/70908.html
+# 日付や日時を扱う
 
-function time::iso8601::now {
+## 本日の日時をISO8601形式で表示する
+function datetime::iso8601 {
 
     date +%Y-%m-%dT%H:%M:%S:%z
     #date -Iseconds
 
 }
 
-function time::iso8601::yesterday {
-    date '+%Y-%m-%d' --date 'yesterday'
-    #date -I --date 'yesterday'
+## 昨日の日付をISO8601形式で表示する
+function datetime::iso8601::yesterday {
+
+    date +%Y-%m-%d --date 'yesterday'
+    # date -I --date 'yesterday'
+    # date -v -1d +%Y-%m-%d
+
 }
 
-function time::iso8601::today {
-    date '+%Y-%m-%d'
-    #date -I
+## 本日の日付をISO8601形式で表示する
+function datetime::iso8601::today {
+
+    date +%Y-%m-%d
+    # date -I
+    # date +%Y-%m-%d
+
 }
 
-function time::iso8601::tomorrow {
-    date '+%Y-%m-%d' --date 'tomorrow'
-    #date -I --date 'tomorrow'
+## 翌日の日付をISO8601形式で表示する
+function datetime::iso8601::tomorrow {
+
+    date +%Y-%m-%d --date 'tomorrow'
+    # date -I --date 'tomorrow'
+    # date -v +1d +%Y-%m-%d
+
 }
