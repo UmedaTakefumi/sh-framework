d86b02e798548690ded7151a03ae2f06bca00393
UmedaTakefumi <ut+github@azurestone.net>
2019-03-28 03:46:29 +0900

    log::debug を実装

--- sample/sample_log.bash
+++ sample/sample_log.bash
@@ -9,3 +9,20 @@ log::info "print foo bar"
 export sh_log_color=off
 echo "sh_log_color: $sh_log_color"
 log::info "print foo bar"
+
+export sh_debug=on
+export sh_log_color=on
+echo "sh_log _color: $sh_log_color"
+log::debug "print foo bar"
+
+export sh_log_color=off
+echo "sh_log _color: $sh_log_color"
+log::debug "print foo bar"
+
+# 表示されないことの検証
+echo "------"
+export sh_debug=""
+log::debug "print foo bar"
+echo "------"
+
+
--- src/log.bash
+++ src/log.bash
@@ -24,11 +24,19 @@ function log::info {
 }
 
 ## 
-#function log::debug {
-#    echo ""
-#}
+function log::debug {
+    if [ ${sh_log_color} = "on" ] && [ ${sh_debug} = "on" ]; then
 
-## 
-#function log::warn {
-#    echo ""
-#}
+        COLOR_CYAN="\033[36m"
+        COLOR_RESET="\x1b[39;49;00m"
+
+        OPT_1=$1
+
+        echo -e "${COLOR_CYAN}${OPT_1}${COLOR_RESET}"
+    
+    elif [ ${sh_log_color} = "off" ] && [ ${sh_debug} = "on" ]; then
+
+        echo ${OPT_1}
+
+    fi
+}
